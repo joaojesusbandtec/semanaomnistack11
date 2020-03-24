1b1cf48193deb1eb9bdd8f98795debe7715924e9
João Marcos <joao.jesus@bandtec.com.br>
2020-03-24 15:46:25 -0300

    Criando a base da aplicação - Construindo o back-end

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -1,12 +1,11 @@
 const express = require('express'); // importando as funcionalidades do express, importando o módulo express para dentro da variável express
+const cors = require('cors');
+const routes = require('./routes'); // importando a variável routes de routes.js
 
 const app = express(); // criando variável que armazena a aplicação, instanciando aplicação
 
-app.get('/', (request, response) => {
-    return response.json({
-        evento: 'Semana OmniStack 11.0',
-        aluno: 'João Marcos'
-    })
-});
+app.use(cors());
+app.use(express.json()); // para entender o corpo e seu formato JSON
+app.use(routes);
 
 app.listen(3333); // mandando a aplicação ouvir a porta 3333
---
+++ aulas/backend/src/routes.js
@@ -0,0 +1,68 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+/**
+ * Rota / Recurso
+*/
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+*/
+
+/**
+ * Tipos de parâmetros: 
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+*/
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+*/
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+*/
+
+/* routes.post('/users/', (request, response) => {
+    //    const params = request.query; // Acessar todos os parâmetros que vem através dos query
+    //    const params = request.params;
+    
+    //   console.log(params);
+    
+       const body = request.body;
+    
+       console.log(body);
+    
+        return response.json({
+            evento: 'Semana OmniStack 11.0',
+            aluno: 'João Marcos'
+        })
+}); */
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes; // exportando a variável das rotas 
