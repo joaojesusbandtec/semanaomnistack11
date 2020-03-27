57900bfb0ff0ee0e7095b215b0460e7a989f4594
João Marcos <joao.jesus@bandtec.com.br>
2020-03-27 14:57:34 -0300

    Funcionalidades Avançadas - Validações e Testes

--- aulas/backend/src/app.js
+++ aulas/backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express'); // importando as funcionalidades do express, importando o módulo express para dentro da variável express
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes'); // importando a variável routes de routes.js
 
 const app = express(); // criando variável que armazena a aplicação, instanciando aplicação
@@ -7,5 +8,6 @@ const app = express(); // criando variável que armazena a aplicação, instanci
 app.use(cors());
 app.use(express.json()); // para entender o corpo e seu formato JSON
 app.use(routes);
+app.use(errors());
 
-app.listen(3333); // mandando a aplicação ouvir a porta 3333
+module.exports = app;
